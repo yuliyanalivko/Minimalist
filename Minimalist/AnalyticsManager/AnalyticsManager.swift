64c35eb8e9eb3f6b029ecfc72d0ecd798ed5b7d4
yuliyanaliuka <julianalivko@gmail.com>
2026-06-04 18:22:39 +0300

    Add addProvider and removeProvider methods to the AnalyticsManager

--- Minimalist/AnalyticsManager/AnalyticsManager.swift
+++ Minimalist/AnalyticsManager/AnalyticsManager.swift
@@ -10,6 +10,20 @@ final class AnalyticsManager {
         self.providers = providers
     }
     
+    func addProvider(_ provider: any AnalyticsTracking) {
+        guard !providers.contains(where: {
+            type(of: $0) == type(of: provider)
+        }) else {
+            return
+        }
+        
+        providers.append(provider)
+    }
+    
+    func removeProvider(ofType providerType: AnalyticsTracking.Type) {
+        providers.removeAll { type(of: $0) == providerType }
+    }
+    
     func trackScreen(_ screenName: String) {
         providers.forEach({ provider in
             guard let provider = provider as? ScreenTracking else {
--- MinimalistTests/AnalyticsManager/AnalyticsManagerTests.swift
+++ MinimalistTests/AnalyticsManager/AnalyticsManagerTests.swift
@@ -18,7 +18,58 @@ final class MockEventTracker<ExpectedEvent>: AnalyticsTracking, EventTracking {
     }
 }
 
+struct MockProviderA: AnalyticsTracking, Equatable {}
+struct MockProviderB: AnalyticsTracking, Equatable {}
+
 struct AnalyticsManagerTests {
+    
+    @Test("Should set new providers value")
+    func updateProviders_setNewValue() {
+        let manager = AnalyticsManager(providers: [])
+        
+        let provider = MockProviderA()
+        manager.updateProviders([provider])
+        
+        #expect(manager.providers.first as? MockProviderA == provider)
+    }
+    
+    @Test("Should add new provider when new provider type is unique")
+    func addProvider_addNewValue_uniqueType() {
+        let manager = AnalyticsManager(providers: [MockProviderA()])
+        
+        let provider = MockProviderB()
+        manager.addProvider(provider)
+        
+        #expect(manager.providers.count == 2)
+    }
+    
+    @Test("Should not add new provider when the provider with the same type exists")
+    func addProvider_ignoreNewProvider_notUniqueType() {
+        let manager = AnalyticsManager(providers: [MockProviderA()])
+        
+        let provider = MockProviderA()
+        manager.addProvider(provider)
+        
+        #expect(manager.providers.count == 1)
+    }
+    
+    @Test("Should remove a provider when the provider with the same type exists")
+    func removeProvider_updateArray_existedType() {
+        let manager = AnalyticsManager(providers: [MockProviderA()])
+        
+        manager.removeProvider(ofType: MockProviderA.self)
+        
+        #expect(manager.providers.count == 0)
+    }
+    
+    @Test("Should not remove a provider when there's no providers with such type")
+    func removeProvider_ignore_uniqueType() {
+        let manager = AnalyticsManager(providers: [MockProviderA()])
+        
+        manager.removeProvider(ofType: MockProviderB.self)
+
+        #expect(manager.providers.count == 1)
+    }
     
     @Test("Tracking a screen routes to the correct provider")
     func trackScreen_triggerRightProvider() {
@@ -37,12 +88,12 @@ struct AnalyticsManagerTests {
         let stringEventTrackerMock = MockEventTracker<String>()
         let manager = AnalyticsManager(providers: [firebaseEvetTrackerMock, stringEventTrackerMock])
         let event = AddToCart(id: "1", name: "Sofa")
-
+        
         manager.logEvent(event)
         
         guard let event = firebaseEvetTrackerMock.loggedEvent as? AddToCart else {
             Issue.record("Expected the first logged event to be AddToCart")
-           
+            
             return
         }
         
